e7fb5e66ce81bbc326b7bb97bd3f912d547587c7
Jose Rodriguez <jose0797@gmail.com>
2020-03-27 16:56:18 +0000

    upgradable jenkins

--- docker/jenkins/run.sh
+++ docker/jenkins/run.sh
@@ -1,2 +1,6 @@
 source ~/.bashrc
-docker run -d  -v /mnt/jenkins/var/jenkins_home:/var/jenkins_home -p 8081:8081 jenkins-website --httpPort=-1 --httpsPort=8081 --httpsKeyStore=/var/jenkins_home/website.jks --httpsKeyStorePassword=$KEYSTORE_PASSWORD
+docker build -t jenkins-website .
+previous_container="$(docker ps -aq --filter 'name=jenkins-website')"
+docker stop $previous_container
+docker rm $previous_container
+docker run -dit  -v /mnt/jenkins/var/jenkins_home:/var/jenkins_home -p 50000:50000 -p 8081:8081 --name jenkins-website -e JAVA_OPTS="-Xmx512m" jenkins-website --httpPort=-1 --httpsPort=8081 --httpsKeyStore=$KEYSTORE_LOCATION --httpsKeyStorePassword=$KEYSTORE_PASSWORD

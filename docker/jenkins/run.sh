b738b0caefd7206ce746317815e3c33d598c33be
Jose Rodriguez <jose0797@gmail.com>
2020-03-28 05:56:46 +0000

    Fixed jenkins upgrade

--- docker/jenkins/run.sh
+++ docker/jenkins/run.sh
@@ -1,6 +1,8 @@
 source ~/.bashrc
+source ~/.secret
+docker pull jenkins/jenkins:latest
 docker build -t jenkins-website .
 previous_container="$(docker ps -aq --filter 'name=jenkins-website')"
 docker stop $previous_container
 docker rm $previous_container
-docker run -dit  -v /mnt/jenkins/var/jenkins_home:/var/jenkins_home -p 50000:50000 -p 8081:8081 --name jenkins-website -e JAVA_OPTS="-Xmx512m" jenkins-website --httpPort=-1 --httpsPort=8081 --httpsKeyStore=$KEYSTORE_LOCATION --httpsKeyStorePassword=$KEYSTORE_PASSWORD
+docker run -dit  -v /mnt/jenkins/var/jenkins_home:/var/jenkins_home -p 50000:50000 -p 8081:8081 --name jenkins-website -e JAVA_OPTS="-Xmx1000m" jenkins-website --httpPort=-1 --httpsPort=8081 --httpsKeyStore=$KEYSTORE_LOCATION --httpsKeyStorePassword=$KEYSTORE_PASSWORD
